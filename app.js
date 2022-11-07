a8c952bcf1c3e8469458182223e1367bc368accc
kinokomush <102983780+kinokomush@users.noreply.github.com>
2022-11-07 18:46:56 +0900

    render js file fixed

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2f831cb3d4aac02393aa';
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2e88e55e58f6abf70757';
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '52cadeeb6f90dd0e4948c40ef9731c5f78e83bbe';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
